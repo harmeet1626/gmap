e372e3c137760f8f90470762a997da05e27e4d05
harmeet1626 <harmeet_singh1@softprodigy.com>
2022-10-01 15:25:29 +0530

    first push

---
+++ src/components/userlocation.vue
@@ -0,0 +1,72 @@
+<template>
+  <div>
+    <h2>Vue Js Google Maps with Multiple Markers Example</h2>
+
+    <gmap-map :center="center" :zoom="15" style="width: 100%; height: 555px">
+      <gmap-marker
+        :key="index"
+        v-for="(gmp, index) in locations"
+        :position="gmp"
+      ></gmap-marker>
+      <GmapPolyline :path="paths"> </GmapPolyline>
+    </gmap-map>
+  </div>
+</template>
+ 
+<script>
+export default {
+  name: "userlocation-",
+  data() {
+    return {
+      center: {
+        lat: 0,
+        lng: 0,
+      },
+      locations: [],
+      currentLocation: null,
+      paths: [
+        {
+          lat: 30.681445812915797,
+          lng: 76.72593267775184,
+          label: "SoftProdigy",
+        },
+
+        {
+          lat: 32.103926752182645,
+          lng: 76.53623763170091,
+          label: "Home",
+        },
+      ],
+    };
+  },
+
+  mounted() {
+    this.setLocationLatLng();
+  },
+
+  methods: {
+    setLocationLatLng() {
+      navigator.geolocation.getCurrentPosition((geolocation) => {
+        this.center = {
+          lat: geolocation.coords.latitude,
+          lng: geolocation.coords.longitude,
+        };
+      });
+
+      this.locations = [
+        {
+          lat: 30.681445812915797,
+          lng: 76.72593267775184,
+          label: "SoftProdigy",
+        },
+
+        {
+          lat: 32.103926752182645,
+          lng: 76.53623763170091,
+          label: "Home",
+        },
+      ];
+    },
+  },
+};
+</script>
